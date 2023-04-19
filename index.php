bffe2493deaa2cc1afb911af22e8594ed4e3c2c8
Renê <81319888+RenePadua@users.noreply.github.com>
2023-04-19 01:51:46 -0300

    Upload de modificações feitas

---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<?php
+session_start();
+require 'dbcon.php';
+?>
+
+
+
+<!doctype html>
+<html lang="pt-br">
+  
+<head>
+  <title>Minha aplicação</title>
+</head>
+<body>
+
+<?php include('header.php'); ?>
+
+
+
+<div id="conteudo">
+  <?php
+    if(isset($_GET['pagina'])){
+      $pagina = $_GET['pagina'];
+      if($pagina == 'about'){
+        include('about.php');
+      } else {
+        include('home.php');
+      }
+    } else {
+      include('home.php');
+    }
+  ?>
+</div>
+
+	
+	
+  </body>
+</html>
---
+++ sobre.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Fatec Parking</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+	
+  </head>
+  <body>
+  
+ <?php include('header.php'); ?>
+
+  
+<div class="container-fluid text-center">
+<h1>#Projeto Fatec Park</h1>
+
+<h2>#Integrantes:</h2>
+
+<p>Renê Silva Amorim de Pádua</p>
+<p>RA - 0050831921023</p>
+<p>Thaís Barbosa da Silva</p>
+<p>RA - 0050831921028</p>
+
+</div>
+
+  </body>
+</html>
+
